3198541b055e674361bffd0331d578a99402e8c2
jaskaran <jaskaran.sarkaria@googlemail.com>
2021-06-09 19:49:56 +0100

    test: 💍 add error case

--- src/fizzBuzz/README.md
+++ src/fizzBuzz/README.md
@@ -5,3 +5,4 @@ Suppose we have a number n. We have to display a string representation of all nu
 *  If the number is divisible by 3, write Fizz instead of the number
 *  If the number is divisible by 5, write Buzz instead of the number
 *  If the number is divisible by 3 and 5 both, write FizzBuzz instead of the number
+*  If the number is NOT divisible by 3 or 5, output the number as a string
--- src/fizzBuzz/index.spec.ts
+++ src/fizzBuzz/index.spec.ts
@@ -1,7 +1,32 @@
 import test, { ExecutionContext } from 'ava';
-import fizzBuzz from './index';
+import processFizzBuzzArr from './index';
 
-test('GIVEN a fizzBuzz function WHEN the function returns THEN it should return true', (t: ExecutionContext) => {
-    t.is(fizzBuzz(), true);
-    // t.pass();
+test(`GIVEN a processFizzBuzzArr function WHEN the argument is divisible by 3 THEN
+  it should return "Fizz"`, (t: ExecutionContext) => {
+    t.deepEqual(processFizzBuzzArr([3, 6, 9]), ['Fizz', 'Fizz', 'Fizz']);
+});
+
+test(`GIVEN a processFizzBuzzArr function WHEN the argument is divisible by 5 THEN 
+  it should return "Buzz"`, (t: ExecutionContext) => {
+    t.deepEqual(processFizzBuzzArr([5, 10, 20]), ['Buzz', 'Buzz', 'Buzz']);
+});
+
+test(`GIVEN a processFizzBuzzArr function WHEN the argument is divisible by 5 or
+  5 THEN it should return "FizzBuzz"`, (t: ExecutionContext) => {
+    t.deepEqual(processFizzBuzzArr([15, 30, 45]), ['FizzBuzz', 'FizzBuzz', 'FizzBuzz']);
+});
+
+test(`GIVEN a processFizzBuzzArr function WHEN the argument is NOT divisible by 3 or
+  5 THEN it should return the number as a string`, (t: ExecutionContext) => {
+    t.deepEqual(processFizzBuzzArr([1, 2, 4]), ['1', '2', '4']);
+});
+
+test(`GIVEN a processFizzBuzzArr function WHEN the argument is varied THEN return
+  the correct value`, (t: ExecutionContext) => {
+    t.deepEqual(processFizzBuzzArr([1, 3, 5, 2, 4, 150]), ['1', 'Fizz', 'Buzz', '2', '4', 'FizzBuzz']);
+});
+
+test(`GIVEN a processFizzBuzzArr function WHEN the argument is an empty array THEN
+  return an empty array`, (t: ExecutionContext) => {
+    t.deepEqual(processFizzBuzzArr([]), []);
 });
