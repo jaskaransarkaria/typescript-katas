2d21a935c5848056a6f810b4d430df6dd8eb3543
jaskaran <jaskaran.sarkaria@googlemail.com>
2021-06-09 19:49:56 +0100

    test: 💍 add error case

--- src/fizzBuzz/index.ts
+++ src/fizzBuzz/index.ts
@@ -1,3 +1,16 @@
-const fizzBuzz = (): boolean => true;
+const isFizzBuzz = (num: number): string => {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
+    }
+    if (num % 3 === 0) {
+        return 'Fizz';
+    }
+    if (num % 5 === 0) {
+        return 'Buzz';
+    }
+    return num.toString();
+};
 
-export default fizzBuzz;
+const processFizzBuzzArr = (numArr: number[]): string[] => numArr.map((num) => isFizzBuzz(num));
+
+export default processFizzBuzzArr;
